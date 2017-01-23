fa9fe034f7b9634033eb48fe038d6f5294f3fa20
mkbaker <mattbakerk@gmail.com>
2017-01-22 18:42:44 -0600

    Initial commit.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        //Player spawn point
+        transform.position = new Vector2(-1, 0.5f);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
