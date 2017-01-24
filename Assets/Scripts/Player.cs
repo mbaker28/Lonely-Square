eb9a32359bf49ae5dd835f26cd3d6e2a855c6047
mkbaker <mattbakerk@gmail.com>
2017-01-23 19:05:20 -0600

    [Re #1] Removed rotation. Added cleanup. Resolves Issue #1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,16 @@ using System.Collections;
 public class Player : MonoBehaviour {
 
     public float speed = 0f;
-    public Vector2 jumpHeight;
     public float rotationSpeed = 0f;
+    public float jumpHeight = 0f;
 
     private float movex = 0f;
     private float movey = 0f;
 
     public bool isGrounded = false;
-    public Transform groundCheck;
-    float groundRadius = 0.2f;
+    public Transform groundCheck1;
+    public Transform groundCheck2;
+
     public LayerMask whatIsGround;
 
     private Rigidbody2D rigi;
@@ -31,13 +32,18 @@ public class Player : MonoBehaviour {
 
     // Update is called once per frame
     void FixedUpdate()
-    {
+    { 
+        isGrounded = Physics2D.OverlapArea(groundCheck1.position, groundCheck2.position, whatIsGround);
+
         movex = Input.GetAxis("Horizontal");
         movey = Input.GetAxis("Vertical");
 
         //move player left and right
         rigi.velocity = new Vector2(movex * speed, rigi.velocity.y);
 
-        transform.Rotate(Vector3.forward, movex * -rotationSpeed);
+        if (isGrounded == true && Input.GetKey(KeyCode.W))
+        {
+            rigi.velocity = new Vector2(rigi.velocity.x, jumpHeight);
+        }
     }
 }
